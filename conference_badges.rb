a72d54c691fdaa36321c85cd4481255a082bf84a
Leonardo Peralta <leo.pm15@gmail.com>
2019-01-18 05:10:26 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.collect do |name|
+    badge_maker(name)
+  end
+end
+
+def assign_rooms(attendees)
+  room_num = 0
+    attendees.collect do |name|
+  room_num += 1
+    "Hello, #{name}! You'll be assigned to room #{room_num}!"
+    end
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |ret_value|
+    puts ret_value
+  end
+  assign_rooms(attendees).each do |ret_value|
+    puts ret_value
+  end
+end
